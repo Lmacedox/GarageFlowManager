7e68516b8e08e0c83ad64d96e7ea132dcd478a78
Lucas Macedo <lucasmacedo376@gmail.com>
2024-10-01 21:17:04 -0300

    feat: create authentication page and useAuthHook

---
+++ src/pages/SignIn/signIn.tsx
@@ -0,0 +1,40 @@
+import { Typography } from '@/components/typography';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+import { InputErrorMessage } from '@/components/input-error-message';
+import { useSignIn } from './use-signIn';
+import { LoaderCircle } from 'lucide-react';
+
+export function SignIn() {
+  const { onSubmit, register, errors, isPending } = useSignIn();
+
+  return (
+    <div className="w-full text-center">
+      <Typography.H2>Acessar Painel</Typography.H2>
+      <Typography.P className="text-muted-foreground">
+        Acompanhe o fluxo da sua garagem!
+      </Typography.P>
+
+      <form onSubmit={onSubmit} className="mt-11 flex flex-col gap-2">
+        <div className="space-y-5 text-left">
+          <Input placeholder="Nome do cliente" {...register('userName')} />
+          <InputErrorMessage inputName={'userName'} errors={errors} />
+        </div>
+
+        <div className="space-y-5 text-left">
+          <Input
+            placeholder="Senha"
+            type="password"
+            {...register('password')}
+          />
+          <InputErrorMessage inputName={'password'} errors={errors} />
+        </div>
+
+        <Button className="mt-1 w-full" disabled={isPending}>
+          {isPending ? <LoaderCircle className="animate-spin" /> : 'Entrar'}
+        </Button>
+      </form>
+    </div>
+  );
+}
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Toaster } from '@/components/ui/toaster';
 
 export function AuthLayout() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex w-full max-w-96 flex-col items-center justify-center">
         <Outlet />
+        <Toaster />
       </div>
     </div>
   );
